9ced897bb2dd8ddadf46e175d2bb001e56b3400d
Kubo2 <kelerest123@gmail.com>
2014-01-14 21:26:39 +0100

    Added phpDoc to describe exception's error level constants. Removed AbstractClass constant.
    phpDoc gets better look of code when someone else sees it. AbstractClass constant wasn't needed.

--- includes/ClassException.php
+++ includes/ClassException.php
@@ -10,14 +10,36 @@
  */
 
 class ClassException extends Exception {
+	/**
+	 * Exception error level code constants.
+	 *
+	 * @var int
+	 */
+	/**
+	 * Undefined kind of problem when throwing with this error level code.
+	 */
 	const Undefined = 1;
 	
+	/**
+	 * When someone attempts to instantiate singleton more times.
+	 */
 	const Singleton = 1.8e1;
+	/**
+	 * When someone attempts to instantiate static class.
+	 */
 	const ClassUtility =  1.8e2;
-	const AbstractClass = 1.8e3;
 
+	/**
+	 * When someone attempts to call class method, that this class doesn't declare.
+	 */
 	const UndeclaredMethod = 1.9e1;
 
+	/**
+	 * Calls parent's constructor with optional $message and $code parameters.
+	 *
+	 * @param string Message shrotly describing what happened | default NULL
+	 * @param int Exception error level code. This is useful when automatically catching exceptions | default 1
+	 */
 	public function __construct($message = NULL, $code = self::Undefined) {
 		parent::__construct($message, $code);
 	}
