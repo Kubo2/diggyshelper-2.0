cd31b897e12bdb5d99c8648e5ebf6faa7f19fec1
Kubo2 <kelerest123@gmail.com>
2014-01-15 01:09:53 +0100

    Constants containing project paths and urls/uris moved from loader to Config class.
    Such a mess, that global constants did, is cleaned up now.

--- includes/load.php
+++ includes/load.php
@@ -6,27 +6,6 @@
  * @package DiggysHelper
  */
 
-/**
- * Constant containing absolute path to our project/app directory IN LOCAL FILESYSTEM.
- *
- * @var string
- * @access public
- */
-define('PROJECT_LOCATION', defined("__DIR__") ? dirname(__DIR__) : dirname(dirname(__FILE__)));
-
-/**
- * Constant containing absoulute web URI to our project's LOCATION (with http:// prefix).
- *
- * @var string
- * @access public
- */
-define('PROJECT_URI',
-	'http://'
-	. (!empty($_SERVER["HTTP_HOST"]) ? $_SERVER["HTTP_HOST"] : $_SERVER["SERVER_NAME"])
-	. (preg_replace("~/public/.*$~", '', dirname($_SERVER["PHP_SELF"])))
-	. '/'
-);
-
 // Prevents from searching for files in more directories
 set_include_path(".");
 
