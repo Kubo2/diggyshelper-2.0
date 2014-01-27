6e05b6679dc62431434576b1f46b33cecbe40eb0
Kubo2 <kelerest123@gmail.com>
2014-01-27 01:29:22 +0100

    IO does not implement the ArrayAcces and Iterator interfaces. IOInput get some basic functionality.

--- includes/IO.class.php
+++ includes/IO.class.php
@@ -8,82 +8,11 @@
  * @package DiggysHelper
  */
 
-abstract class IO implements ArrayAccess, Iterator {
+abstract class IO {
 	/**
 	 * Buffer for storing output templates.
 	 *
 	 * @var Array of strings
 	 */
 	protected static $buffer = array();
-
-	/**
-	 * ArrayAccess::offsetExists()
-	 *
-	 * @param mixed $offset
-	 * @return bool
-	 */
-	abstract public function offsetExists($offset);
-
-	/**
-	 * ArrayAccess::offsetUnset()
-	 *
-	 * @param mixed $offset
-	 */
-	abstract public function offsetUnset($offset);
-
-	/**
-	 * ArrayAccess::offsetGet()
-	 *
-	 * @param mixed $offset
-	 * @return mixed
-	 */
-	abstract public function offsetGet($offset);
-
-	/**
-	 * ArrayAccess::offsetSet()
-	 *
-	 * @param mixed
-	 * @param mixed
-	 */
-	abstract public function offsetSet($name, $value);
-
-	/**
-	 * Iterator::rewind()
-	 * Move to first position (start position of an array).
-	 *
-	 * @return void
-	 */
-	abstract public function rewind();
-	
-	/**
-	 * Iterator::current()
-	 * Returns current value.
-	 *
-	 * @return mixed
-	 */
-	abstract public function current();
-
-    /**
-     * Iterator::key()
-     * Index for current position.
-     *
-     * @return mixed
-     */
-    abstract public function key();
-
-    /**
-     * Iterator::next()
-     * Move to next field.
-     *
-     * @return void
-     */
-    abstract public function next();
-
-    /**
-     * Iterator::valid()
-     * Whether exists current offset.
-     *
-     * @return boolean
-     */
-    abstract public function valid();
 }
--- includes/IOInput.class.php
+++ includes/IOInput.class.php
@@ -17,5 +17,54 @@ class IOInput extends IO {
 	public function __construct() {
 		throw new ClassException(__CLASS__ . "is static class. You cannot instantiate it.", ClassException::ClassUtility);
 	}
-	
+
+	/**
+	 * Finds out HTTP method used for current request.
+	 *
+	 * @return string HTTP Request method
+	 */
+	public static function getMethod() {
+		return $_SERVER['REQUEST_METHOD'];
+	}
+
+	/**
+	 * Populates raw post data.
+	 *
+	 * @return string raw post data
+	 */
+	public static function getRawInput() {
+		static $data;
+		if(!empty($data))
+			$data = self::getMethod() != "POST" || !file_exists("php://input") ? false : file_get_contents("php://input");
+		return $data;
+	}
+
+	/**
+	 * Provides universal way for accessing GET/POST data depending on request method
+	 *
+	 * @param string name of GET/POST var
+	 * @return mixed value of GET/POST var
+	 */
+	public static function getInput($name) {
+			return isset($_GET[$name]) ? $_GET[$name] : (isset($_POST[$name]) ? $_POST[$name] : false);
+	}
+
+	/**
+	 * Provides access to GET vars.
+	 *
+	 * @param string name of the var, optional
+	 * @return string|array
+	 */
+	public static function getQsVar($name = false) {
+		if(!$name)
+			return $_GET;
+		return isset($_GET[$name]) ? $_GET[$name] : false;
+	}
+
+	/**
+	 * Provides access to POST vars.
+	 *
+	 * @param string varname, optional
+	 * @return string|array
+	 */
 }
