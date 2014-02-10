5fa68b266f862927f0a51b6cce9a7243fa811ad4
Kubo2 <kelerest123@gmail.com>
2014-02-10 19:15:42 +0100

    index.php turns into front controller.

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 /**
  * Diggy's helper website.
+ * Front controller, dispatcher bootstrap.
+ * Based on partially modified MVC framework.
  * @author Jakub Kubíček <kelerest123@gmail.com>
  * @author Vladimír Jacko <vladimir.jacko.ml@gmail.com>
  * @version 2.0
@@ -11,3 +13,9 @@
  */
 
 require "./includes/load.php";
+
+try {
+	Controller::init();
+} catch(ClassNotFoundException $e) {
+
+}
