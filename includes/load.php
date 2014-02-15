f1bbea14bdef54875daff71ec928701d1a7b2383
Kubo2 <kelerest123@gmail.com>
2014-02-15 16:39:18 +0100

    ClassNotFoundException now inherits ClassException instead of Exception.

--- includes/load.php
+++ includes/load.php
@@ -24,7 +24,7 @@ define('PROJECT_LOCATION', defined("__DIR__") ? dirname(__DIR__) : dirname(dirna
  *
  * @var string
  */
-define('PROJECT_URL', rtrim(dirname($_SERVER["PHP_SELF"]), DIRECTORY_SEPARATOR) . '/');
+define('PROJECT_URL', rtrim(dirname($_SERVER["SCRIPT_NAME"]), DIRECTORY_SEPARATOR) . '/');
 
 /**
  * Constant containing absoulute web URI to our project's LOCATION (with http:// prefix).
@@ -36,7 +36,7 @@ define('PROJECT_URI',
 	. (isset($_SERVER["HTTPS"]) && strtoupper($_SERVER["HTTPS"]) != "OFF" ? 's' : '')
 	. '://'
 	. (!empty($_SERVER["HTTP_HOST"]) ? $_SERVER["HTTP_HOST"] : $_SERVER["SERVER_NAME"])
-	. rtrim(preg_replace("~/public/.*$~", '', dirname($_SERVER["PHP_SELF"])), DIRECTORY_SEPARATOR)
+	. rtrim(preg_replace("~/public/.*$~", '', dirname($_SERVER["SCRIPT_NAME"])), DIRECTORY_SEPARATOR)
 	. '/'
 );
 
@@ -51,20 +51,34 @@ require PROJECT_LOCATION . "/vendor/autoload.php";
  */
 
 function __autoload($className) {
-	if(!error_reporting() || !(int)ini_get('error_reporting')) {
-		(1 === include(PROJECT_LOCATION . "/includes/$className.class.php"))
-		|| (1 === include(PROJECT_LOCATION . "/includes/$className.inc.php"))
-		|| (1 === include(PROJECT_LOCATION . "/includes/traits/$className.php"))
-		|| (include(PROJECT_LOCATION . "/includes/$className.php"));
-	} else {
-		(1 === @include(PROJECT_LOCATION . "/includes/$className.class.php"))
-		|| (1 === @include(PROJECT_LOCATION . "/includes/$className.inc.php"))
-		|| (1 === @include(PROJECT_LOCATION . "/includes/traits/$className.php"))
-		|| (@include(PROJECT_LOCATION . "/includes/$className.php"));
+	static $paths = [ "%s.php", "controllers/%s.inc.php", "%s.class.php", "%s.inc.php", "traits/%s.php" ];
+	foreach ($paths as $path) {
+		$fullPath = PROJECT_LOCATION . sprintf("/includes/$path", $className);
+		if(file_exists($fullPath)) {
+			require $fullPath;
+			return;
+		}
 	}
+	throw new ClassNotFoundException;
 }
 
 spl_autoload_register('__autoload');
+
+/**
+ * Defines default implementation of ClassNotFoundException.
+ * @access public
+ */
+final class ClassNotFoundException extends ClassException {
+	// empty implementation
+}
+
+/**
+ * Defines default (empty) implementation of InvalidMethodForControllerException.
+ * @access public
+ */
+final class InvalidMethodForControllerException extends Exception {
+
+}
 
 // includes configuration class
 // @since 2.0.0.1 will be included automatically by __autoload() implementation
