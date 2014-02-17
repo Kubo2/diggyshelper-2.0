fb1f711ed7dafb95fbe664364a9e9562d57aeb5c
Kubo2 <kelerest123@gmail.com>
2014-02-17 21:58:07 +0100

    Front controller got reprogrammed.
    Now it can also catch and parse exceptions.

--- index.php
+++ index.php
@@ -16,7 +16,21 @@
 require "./includes/load.php";
 
 try {
-	Controller::init();
-} catch(ClassNotFoundException $e) {
-
+	if(isset($_GET['js'])) {
+		Controller::init( [ 'ajax_request' => true ] );
+	} else {
+		Controller::init();
+	}
+} catch(Exception $e) {
+	@ob_end_clean();
+	@header("Retry-After: 604800", true, 503);
+	@header("Connection: close");
+?>
+<!DOCTYPE html><html><head><meta charset="utf-8">
+<title>503 Service Unavailable</title>
+</head><body><h1>503 Web nedostupný</h1><p>
+Server vrátil stavový kód 503 oznamujúci dočasnú nedostupnosť webu.<br>
+Pravdepodobne nastala fatálna chyba v aplikácii alebo prebieha údržba.
+Skúste sa sem prosím vrátiť neskôr.</p></body></html>
+<?php
 }
