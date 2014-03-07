864f12052e02fdb38784f2f3ad5527c7c2c6838b
Kubo2 <kelerest123@gmail.com>
2014-03-07 23:33:58 +0100

    Created custom child of Smarty class.
    Instance of this child is now returned by View\getConfigured() function.
    It's used by VIEW mechanism, e.g. IOOutput manipulates with it.

--- includes/view/configured.php
+++ includes/view/configured.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace View {
-  function getConfigured() {
-    if(!isset($smarty)) {
-      static $smarty = new  \Smarty();
-    }
-    return clone $smarty;
-  }
+	class DiggysSmarty extends \Smarty {
+		public function __construct() {
+			parent::__construct();
+			$this->caching = true;
+			$this->template_dir = PROJECT_LOCATION . "/html/";
+			$this->compile_dir = PROJECT_LOCATION . "/html/compiled/";
+			$this->config_dir = PROJECT_LOCATION . "/html/config/";
+			$this->cache_dir = PROJECT_LOCATION . "/cache/";
+		}
+	}
+
+	function getConfigured() {
+		if(!isset($smarty)) {
+			static $smarty = new  DiggysSmarty();
+		}
+		return clone $smarty;
+	}
 }
